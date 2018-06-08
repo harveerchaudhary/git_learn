a0c5340af0b9fef5fac620bfb389b61638fe1a1f
harveerchaudhary <harveer7890@gmail.com>
2018-06-08 15:17:46 +0530

    sum list

--- rotate_list.py
+++ rotate_list.py
@@ -1,11 +1,18 @@
 from collections import deque
+#list rotation
 def rotate(arr,k):
 	brr=deque(arr)
 	brr.rotate(k)
-	print(brr)
+	print('rotation of the list k times:',brr)
+def sum_list(arr):
+	print('sum of the list:',sum(arr))
+	
 print('enter the size of list')
 n=int(input())
 arr=list(map(int,input().strip().split(' ')))
 print('enter the no. of rotation:')
 k=int(input())
 rotate(arr,k)
+sum_list(arr)
+
+
